3ed9f23b6e5cdac97314bc6aa8190a6f52c30866
Paul Schaack <paul.schaack2@gmail.com>
2020-04-15 21:04:35 +0100

    GPIO init changes

--- runserver.py
+++ runserver.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+
 from rpiserver import app
 
 app.run(host="0.0.0.0", threaded=True)
+
+
+# GPIO housekeeping
+GPIO.cleanup()
