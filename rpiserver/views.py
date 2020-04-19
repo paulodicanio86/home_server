bf4cfd70949e514b7cc0a53cd1ad1b45278579b9
Paul Schaack <paul.schaack2@gmail.com>
2020-04-19 20:49:40 +0100

    changed pin to name_key

--- rpiserver/views.py
+++ rpiserver/views.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 from flask import Flask, render_template, request, redirect, url_for
 
 
-from rpiserver import app, load_data, path_json, path
+from rpiserver import app, load_data, path_json
 from rpiserver import initiate_pin_states, read_pin_states, write_data, GPIO_on, GPIO_off
 
 
@@ -22,62 +22,56 @@ def main():
     return render_template('main.html', **template_data)
 
 
-@app.route("/<change_pin>/<action>")
-def change_pin_domain(change_pin, action):
+@app.route("/<name_key>/<action>")
+def change_pin_domain(name_key, action):
     initiate_pin_states(path_json)
-    change_pin = int(change_pin)
     pins_in = load_data(path_json)
 
     if action == "on":
-        GPIO.output(change_pin, GPIO_on)
+        GPIO.output(pins_in[name_key]['pin'], GPIO_on)
 
     if action == "off":
-        GPIO.output(change_pin, GPIO_off)
+        GPIO.output(pins_in[name_key]['pin'], GPIO_off)
 
     if action == "duration":
-        sleep_duration = 0
-        for key in pins_in:
-            if pins_in[key]['pin'] == change_pin and 'duration' in pins_in[key].keys():
-                sleep_duration = pins_in[key]['duration']
-        GPIO.output(change_pin, GPIO_on)
+        sleep_duration = pins_in[name_key]['duration']
+        GPIO.output(pins_in[name_key]['pin'], GPIO_on)
         sleep(sleep_duration)
-        GPIO.output(change_pin, GPIO_off)
+        GPIO.output(pins_in[name_key]['pin'], GPIO_off)
 
         # capture last_on time
-        now = datetime.now()
-        now_str = now.strftime("%d/%m/%Y, %H:%M:%S")
-        for key in pins_in:
-            if pins_in[key]['pin'] == change_pin and 'last_on' in pins_in[key].keys():
-                data_in = load_data(path_json)
-                data_in[key]['last_on'] = now_str
-                write_data(path_json, data_in)
+        if 'last_on' in pins_in[name_key].keys():
+            now_str = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+            data_in = load_data(path_json)
+            data_in[name_key]['last_on'] = now_str
+            write_data(path_json, data_in)
 
     return redirect(url_for('main'))
 
 
-@app.route("/edit/<entry>", methods=['GET'])
-def edit_domain_get(entry):
+@app.route("/edit/<name_key>", methods=['GET'])
+def edit_domain_get(name_key):
 
     pins_in = load_data(path_json)
 
     template_data = {
-        'entry': pins_in[entry],
-        'entry_name': entry
+        'entry': pins_in[name_key],
+        'entry_name': name_key
         }
 
-    if pins_in[entry]['mode'] == 'on_off':
+    if pins_in[name_key]['mode'] == 'on_off':
         return render_template('edit_on_off.html', **template_data)
-    elif pins_in[entry]['mode'] == 'duration':
+    elif pins_in[name_key]['mode'] == 'duration':
         return render_template('edit_duration.html', **template_data)
 
 
 @app.route("/edit", methods=['POST'])
 def edit_domain_post():
 
-    entry_name = request.form['entry_name']
+    name_key = request.form['entry_name']
     data_in = load_data(path_json)
 
-    if data_in[entry_name]['mode'] == 'duration':
+    if data_in[name_key]['mode'] == 'duration':
         on_time_1 = request.form['on_time_1']
         on_time_2 = request.form['on_time_2']
         on_time_3 = request.form['on_time_3']
@@ -90,10 +84,10 @@ def edit_domain_post():
             on_times.append(on_time_2)
         if on_time_3 != '':
             on_times.append(on_time_3)
-        data_in[entry_name]['on'] = on_times
-        data_in[entry_name]['duration'] = int(duration)
+        data_in[name_key]['on'] = on_times
+        data_in[name_key]['duration'] = int(duration)
 
-    if data_in[entry_name]['mode'] == 'on_off':
+    if data_in[name_key]['mode'] == 'on_off':
         on_time_1 = request.form['on_time_1']
         on_time_2 = request.form['on_time_2']
         on_time_3 = request.form['on_time_3']
@@ -108,7 +102,7 @@ def edit_domain_post():
             on_times.append(on_time_2)
         if on_time_3 != '':
             on_times.append(on_time_3)
-        data_in[entry_name]['on'] = on_times
+        data_in[name_key]['on'] = on_times
 
         off_times = []
         if off_time_1 != '':
@@ -117,10 +111,10 @@ def edit_domain_post():
             off_times.append(off_time_2)
         if off_time_3 != '':
             off_times.append(off_time_3)
-        data_in[entry_name]['off'] = off_times
+        data_in[name_key]['off'] = off_times
 
     write_data(path_json, data_in)
-    return redirect(url_for('edit_domain_get', entry=entry_name))
+    return redirect(url_for('edit_domain_get', name_key=name_key))
 
 
 
