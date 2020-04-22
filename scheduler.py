57d9e1e6f5c723ab55a5618da3be5771bef7a55b
Paul Schaack <paul.schaack2@gmail.com>
2020-04-22 21:06:22 +0100

    scheduler to write last duration on time

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,4 @@
-import json
+import json, os
 from time import sleep
 from datetime import datetime
 import RPi.GPIO as GPIO
@@ -9,15 +9,27 @@ GPIO_on = GPIO.LOW # =False
 GPIO_off = GPIO.HIGH # =True
 
 
+# Load data from json
+def load_data(file_path):
+    f = open(file_path)
+    loaded = json.load(f)
+    f.close()
+    return loaded
+
+
+def write_data(file_path, to_write):
+    with open(file_path, 'w') as f2:
+        json.dump(to_write, f2, indent=4)
+
+
 now = datetime.now()
 hour = now.strftime("%H")
 minute = now.strftime("%M")
 
-# hour = '12'
-# minute = '30'
+path = os.path.dirname(os.path.abspath(__file__))
+path_json = os.path.join(path, 'schedule.json')
+data = load_data(path_json)
 
-f = open('/home/pi/home_server/schedule.json')
-data = json.load(f)
 
 for entry in data:
     data_dic = data[entry]
@@ -42,6 +54,7 @@ for entry in data:
                 sleep(data_dic['duration'])
                 GPIO.output(data_dic['pin'], GPIO_off)
 
-
-
-
+                # capture last_on time
+                now_str = now.strftime("%d/%m/%Y, %H:%M:%S")
+                data[entry]['last_on'] = now_str
+                write_data(path_json, data)
