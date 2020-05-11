23d9a2bb7309c133adc8b5fe4781ba24fa806b33
Paul Schaack <paul.schaack2@gmail.com>
2020-05-11 12:45:45 +0100

    changed scheduler to be IP friendly

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,7 @@
 import json, os
 from time import sleep
 from datetime import datetime
+import requests
 import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
@@ -22,6 +23,28 @@ def write_data(file_path, to_write):
         json.dump(to_write, f2, indent=4)
 
 
+def turn_pin(pin, state):
+    GPIO.setup(pin, GPIO.OUT)
+    if state == 'ON':
+        GPIO.output(pin, GPIO_on)
+    elif state == 'OFF':
+        GPIO.output(pin, GPIO_off)
+
+
+def turn_ip(ip, state):
+    # state should either be "ON" or "OFF"
+    requests.post("http://" + ip + "/RELAY=" +state)
+
+
+def turn_device(pin, state):
+    #if pin is an integer the GPIO is used
+    if isinstance(pin, int):
+        turn_pin(pin, state)
+    #if pin is a string the IP is used
+    elif isinstance(pin, str):
+        turn_ip(pin, state)
+
+
 now = datetime.now()
 hour = now.strftime("%H")
 minute = now.strftime("%M")
@@ -37,22 +60,19 @@ for entry in data:
     if data_dic['mode'] == 'on_off':
         for on_time in data_dic['on']:
             if on_time.split(':')[0] == hour and on_time.split(':')[1] == minute:
-                GPIO.setup(data_dic['pin'], GPIO.OUT)
-                GPIO.output(data_dic['pin'], GPIO_on)
+                turn_device(data_dic['pin'], 'ON')
 
         for off_time in data_dic['off']:
             if off_time.split(':')[0] == hour and off_time.split(':')[1] == minute:
-                GPIO.setup(data_dic['pin'], GPIO.OUT)
-                GPIO.output(data_dic['pin'], GPIO_off)
+                turn_device(data_dic['pin'], 'OFF')
 
     elif data_dic['mode'] == 'duration':
         for on_time in data_dic['on']:
             if on_time.split(':')[0] == hour and on_time.split(':')[1] == minute:
 
-                GPIO.setup(data_dic['pin'], GPIO.OUT)
-                GPIO.output(data_dic['pin'], GPIO_on)
+                turn_device(data_dic['pin'], 'ON')
                 sleep(data_dic['duration'])
-                GPIO.output(data_dic['pin'], GPIO_off)
+                turn_device(data_dic['pin'], 'OFF')
 
                 # capture last_on time
                 now_str = now.strftime("%d/%m/%Y, %H:%M:%S")
--- write_sample_schedule.py
+++ write_sample_schedule.py
@@ -2,10 +2,10 @@ import json
 
 schedule = {}
 
-schedule['lamp'] = {'name': 'Lamp', 'pin': 7,
+schedule['lamp'] = {'name': 'Lamp', 'pin': '192.168.0.192',
                     'mode': 'on_off', 'on': ['10:00', '11:00'], 'off': ['10:01', '11:01']}
 schedule['water'] = {'name': 'Waterpump', 'pin': 7,
-                     'mode': 'duration', 'on': ['12:00'], 'duration': 7, 'last_on': '?'}
+                     'mode': 'duration', 'on': ['09:30'], 'duration': 4, 'last_on': '?'}
 
 with open('schedule.json', 'w') as f:
     json.dump(schedule, f, indent=4)
